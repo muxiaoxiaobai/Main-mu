53e17029b54583d5af4eb438b391486b6f9aa5d6
Main mu <122475845@qq.com>
2022-08-16 09:35:38 +0800

    上传一个电商购物程序（vue）

---
+++ vue_shop/src/components/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>Welcome!!</h3>
+    <img src="../assets/beijing.png" alt="">
+  </div>
+</template>
+<style scoped>
+  h3{
+    text-align: center;
+  }
+  img{
+    width: 100%;
+    height: 100%;
+  }
+</style>
